d7cd9c4078ad4ee5a0e9e0ad7ba3eb4c086e3695
PlasticSan <lanjack271@gmail.com>
2025-07-30 23:38:39 +0800

    Add project files.

---
+++ FyuuEngine/FyuuEngine.cpp
@@ -0,0 +1,12 @@
+﻿// FyuuEngine.cpp : Defines the entry point for the application.
+//
+
+#include "FyuuEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
